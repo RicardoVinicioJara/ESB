d797ec035732ddd6247e898f14488b2506a34985
Ricardo Vinicio Jara Jara <49212955+RicardoVinicioJara@users.noreply.github.com>
2021-02-09 20:28:13 -0500

    1. Creacion de imagnes 2. Muestra de datos en pantalla

--- Restaurante/app.py
+++ Restaurante/app.py
@@ -1,15 +1,12 @@
-from random import random
 from flask import request, render_template
 import requests
+import urllib.request
+import os
 
 from flask import Flask
 
 app = Flask(__name__)
 DAT = {}
-
-
-def setdatos(d):
-    DAT.update(d)
 
 
 @app.route('/')
@@ -22,14 +19,23 @@ def get_datos():
     return DAT
 
 
+@app.route('/guardarimagen', methods=['POST'])
+def guardar_imagen():
+    url = request.json['url']
+    # url = "https://images.dog.ceo/breeds/labrador/n02099712_3776.jpg"
+    urllib.request.urlretrieve(url, "static/img/perro.jpg")
+    return {'path': os.path.abspath("static/img/perro.jpg")}
+
+
 @app.route('/sendDatos', methods=['POST'])
 def sendDatos():
     if request.method == 'POST':
         id_usr = request.form['id_usr']
+        raza = request.form['raza']
+        dat = {'id_usr': id_usr, "raza": raza}
+        DAT.update(dat)
         url = "http://localhost:8081"
-        dat = {'id_usr': id_usr}
-        setdatos(dat)
         r = requests.get(url=url, params=dat)
-
-        return r.json()
+        r = r.json()
+        return render_template("index.html", datos=r)
     return "no Entro aca"
