9645154cfee094bd13dd74ba87e5ed2eee85ed67
JuanC <49213049+jcuyaguari@users.noreply.github.com>
2021-02-13 02:23:31 -0500

    compras

--- CompraProductos/app.py
+++ CompraProductos/app.py
@@ -1,12 +1,24 @@
 from flask import Flask
-
+import mysql.connector
 app = Flask(__name__)
-
+cnx = mysql.connector.connect(user='root', password='',
+                              host='127.0.0.1',
+                              database='productos')
+cursor = cnx.cursor()
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
+    cant1 = 10
+    cant2 = 20
+    mydict = {}
+    query = (""" select * from producto 
+                 where precio between %s and %s; """)
+    cursor.execute(query, (cant1, cant2))
+    for (ID, NOM, PRE, STK) in cursor:
+        mydict[str(ID)] = {"nombre": NOM, "precio": PRE, "stock": STK}
+        print("{}, {}, {}, {}".format(
+            ID, NOM, PRE, STK))
+    return mydict
 
 if __name__ == '__main__':
     app.run()
